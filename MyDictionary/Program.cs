3ce4dd5d72c82d2c4f7103c447c74fd7624827aa
ysnnymn <yasinyaman985@gmail.com>
2022-10-24 14:26:04 +0300

    .

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<int, string> il = new Dictionary<int, string>();
+            //il.Add(1, "Adana");
+            //il.Add(42, "Konya");
+            //il.Add(43, "Kütahya");
+            //il.Add(46, "KahramanMaraş");
+            //il.Add(65, "Van");
+
+            //Console.WriteLine("Plaka Giriniz:");
+            //int plaka = int.Parse(Console.ReadLine());
+            //try
+            //{
+            //    Console.WriteLine(il[plaka]);
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Girilen Plakaya Ait Bir Şehir Bulunamadı");
+            //}
+
+            mydictionary<int, string> ogrenci = new mydictionary<int, string>();
+            ogrenci.Add(12, "Yasin");
+            ogrenci.Add(18, "Yaman");
+            ogrenci.Add(25, "Çınar");
+            Console.WriteLine(ogrenci.Length);
+            
+           
+
+
+
+        }
+    }
+}
---
+++ MyDictionary/mydictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class mydictionary<K,V>
+    {
+        V[] v;
+        K[] k;
+       
+        public mydictionary()
+        {
+            k = new K[0];
+            v = new V[0];
+        }
+        public void Add(K key,V value)
+        {
+            K[] ktemparray = k;
+            k = new K[k.Length+1];
+            V[] vtemparray = v;
+            v = new V[v.Length + 1];
+            for (int i = 0; i <ktemparray.Length; i++)
+            {
+               
+                k[i] = ktemparray[i];
+                v[i] = vtemparray[i];
+            }
+            v[v.Length - 1] = value;
+            k[k.Length - 1] = key;
+
+        }
+        public int Length
+        {
+            get { return v.Length; }
+          
+        }
+
+    }
+}
